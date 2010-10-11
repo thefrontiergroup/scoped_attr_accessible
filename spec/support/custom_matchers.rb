84713177a593fccc1f8f1917a87e76412a5ea13d
Darcy Laycock and Mario Visic <team+darcy+mario@thefrontiergroup.com.au>
2010-10-11 15:19:00 +0800

    Add custom matchers.

---
+++ spec/support/custom_matchers.rb
@@ -0,0 +1,26 @@
+module CustomMatchers
+  class OnlyHaveAttributesMatcher
+    
+    def initialize(*attrs)  
+      @attributes = attrs.map(&:to_s)
+    end  
+  
+    def matches?(target)  
+      @target_attributes = target.keys.map(&:to_s)
+      @target_attributes == @attributes
+    end  
+  
+    def failure_message_for_should  
+      "expected to only allow attributes #{@attributes.join(", ")}, allowed #{@target_attributes.join(", ")}"
+    end  
+  
+    def failure_message_for_should_not  
+      "expected to not only allow attributes #{@attributes.join(", ")}, allowed #{@target_attributes.join(", ")}"
+    end  
+    
+  end  
+  
+  def only_have_attributes(*attrs)  
+    OnlyHaveAttributesMatcher.new(*attrs)  
+  end  
+end
