0094632817c762c3f45caf31a64da9f19c692522
Darcy Laycock and Mario Visic <team+darcy+mario@thefrontiergroup.com.au>
2010-10-11 15:19:50 +0800

    Hooks, Models and Hacks - Oh My!

---
+++ lib/scoped_attr_accessible/active_model_mixin.rb
@@ -0,0 +1,109 @@
+require 'active_support/concern'
+require 'active_support/core_ext/class/inheritable_attributes'
+require 'active_support/core_ext/array/extract_options'
+
+module ScopedAttrAccessible
+  module ActiveModelMixin    
+    extend ActiveSupport::Concern
+    
+    
+    module IncludedHook
+      def included(base)
+        super
+        base.class_eval { include ActiveModelMixin }
+      end
+    end
+    
+    included do
+      extlib_inheritable_accessor :_scoped_attr_sanitizer
+    end
+    
+    module ClassMethods
+      
+      def attr_accessible(*args)
+        scopes    = scopes_from_args(args)
+        sanitizer = self.scoped_attr_sanitizer
+        args.each do |attribute|
+          scopes.each { |s| sanitizer.make_accessible attribute, s }
+        end
+        self._active_authorizer = sanitizer
+      end
+      
+      def attr_protected(*args)
+        scopes    = scopes_from_args(args)
+        sanitizer = self.scoped_attr_sanitizer
+        args.each do |attribute|
+          scopes.each { |s| sanitizer.make_protected attribute, s }
+        end
+        self._active_authorizer = sanitizer
+      end
+      
+      def scoped_attr_sanitizer
+        self._scoped_attr_sanitizer ||= ScopedAttrAccessible::Sanitizer.new
+      end
+      
+      def current_sanitizer_scope
+        Thread.current[current_sanitizer_scope_key]
+      end
+      
+      def current_sanitizer_scope=(value)
+        Thread.current[current_sanitizer_scope_key] = value
+      end
+      
+      def with_sanitizer_scope(scope_name)
+        old_scope = current_sanitizer_scope
+        self.current_sanitizer_scope = scope_name
+        yield if block_given?
+      ensure
+        self.current_sanitizer_scope = old_scope
+      end
+      
+      def sanitizer_scope_recognizer(name, &recognizer)
+        scoped_attr_sanitizer.define_recognizer(name, &recognizer)
+      end
+      
+      def sanitizer_scope_converter(&converter)
+        scoped_attr_sanitizer.define_converter(&converter)
+      end
+      
+      protected
+      
+      def current_sanitizer_scope_key
+        :"#{name}_sanitizer_scope"
+      end
+      
+      def scopes_from_args(args)
+        options = args.extract_options!
+        scope   = Array(options.delete(:scope)).map(&:to_sym)
+        scope  << :default if scope.empty?
+        args   << options unless options.empty?
+        scope
+      end
+      
+    end
+    
+    module InstanceMethods
+
+      def current_sanitizer_scope
+        @current_sanitizer_scope || self.class.current_sanitizer_scope || :default
+      end
+
+      def current_sanitizer_scope=(value)
+        @current_sanitizer_scope = value
+      end
+
+      protected
+
+      def sanitize_for_mass_assignment(attributes)
+        authorizer = self.mass_assignment_authorizer
+        if authorizer.respond_to?(:sanitize_with_scope)
+          authorizer.sanitize_with_scope attributes, current_sanitizer_scope, self
+        else
+          super
+        end
+      end
+
+    end
+    
+  end
+end
---
+++ spec/scoped_attr_accessible/active_model_mixin_spec.rb
@@ -0,0 +1,314 @@
+require 'spec_helper'
+
+describe ScopedAttrAccessible::ActiveModelMixin do
+  
+  before :all do
+    ScopedAttrAccessible.mixin!
+  end
+  
+  subject { Class.new { include ActiveModel::MassAssignmentSecurity } }
+  
+  let(:subject_instance) { subject.new }
+  
+  context 'attr_accessible overrides' do
+    
+    before :each do
+      @sanitizer = ScopedAttrAccessible::Sanitizer.new
+      stub(subject).scoped_attr_sanitizer { @sanitizer }
+    end
+    
+    it 'should call make_accessible' do
+      mock(@sanitizer).make_accessible(:a, :x)
+      subject.attr_accessible :a, :scope => :x
+    end
+    
+    it 'should default the scope' do
+      mock(@sanitizer).make_accessible(:a, :default)
+      subject.attr_accessible :a
+    end
+    
+    it 'should call it correctly when given multiple scopes' do
+      mock(@sanitizer).make_accessible(:a, :admin)
+      mock(@sanitizer).make_accessible(:a, :normal)
+      subject.attr_accessible :a, :scope => [:admin, :normal]
+    end
+    
+    it 'should call make_accessible for each attribute' do
+      mock(@sanitizer).make_accessible(:a, :default)
+      mock(@sanitizer).make_accessible(:b, :default)
+      subject.attr_accessible :a, :b
+    end
+    
+    it 'should set the active authorizer' do
+      subject._active_authorizer = nil
+      subject.attr_accessible :a, :b
+      subject._active_authorizer.should == @sanitizer
+    end
+    
+  end
+  
+  context 'attr_protected overrides' do
+    
+    before :each do
+      @sanitizer = ScopedAttrAccessible::Sanitizer.new
+      stub(subject).scoped_attr_sanitizer { @sanitizer }
+    end
+    
+    it 'should call make_protected' do
+      mock(@sanitizer).make_protected(:a, :x)
+      subject.attr_protected :a, :scope => :x
+    end
+    
+    it 'should default the scope' do
+      mock(@sanitizer).make_protected(:a, :default)
+      subject.attr_protected :a
+    end
+    
+    it 'should call it correctly when given multiple scopes' do
+      mock(@sanitizer).make_protected(:a, :admin)
+      mock(@sanitizer).make_protected(:a, :normal)
+      subject.attr_protected :a, :scope => [:admin, :normal]
+    end
+    
+    it 'should call make_protected for each attribute' do
+      mock(@sanitizer).make_protected(:a, :default)
+      mock(@sanitizer).make_protected(:b, :default)
+      subject.attr_protected :a, :b
+    end
+    
+    it 'should set the active authorizer' do
+      subject._active_authorizer = nil
+      subject.attr_protected :a, :b
+      subject._active_authorizer.should == @sanitizer
+    end
+    
+  end
+  
+  context 'getting the current sanitizer' do
+    
+    it 'should create a new sanitizer if not present' do
+      subject._scoped_attr_sanitizer = nil
+      @sanitizer = ScopedAttrAccessible::Sanitizer.new
+      mock(ScopedAttrAccessible::Sanitizer).new { @sanitizer }
+      subject.scoped_attr_sanitizer.should equal(@sanitizer)
+    end
+    
+    it 'should memoize the sanitizer' do
+      sanitizer = subject.scoped_attr_sanitizer
+      sanitizer_two = subject.scoped_attr_sanitizer
+      sanitizer.should equal(sanitizer)
+    end
+    
+  end
+  
+  context 'current sanitizer scope' do
+    
+    it 'should let you get the default sanitizer scope' do
+      subject.current_sanitizer_scope.should be_nil
+    end
+    
+    it 'should let you set the sanitizer scope' do
+      subject.current_sanitizer_scope = :a
+      subject.current_sanitizer_scope.should == :a
+      subject.current_sanitizer_scope = :b
+      subject.current_sanitizer_scope.should == :b
+    end
+    
+    it 'should let you temporary change the sanitizer scope' do
+      subject.current_sanitizer_scope = :before
+      inner_called = false
+      subject.with_sanitizer_scope :after do
+        inner_called = true
+        subject.current_sanitizer_scope.should == :after
+      end
+      inner_called.should be_true
+    end
+    
+    it 'should reset the scope after a temporary change' do
+      subject.current_sanitizer_scope = :before
+      inner_called = false
+      subject.with_sanitizer_scope :after do
+        inner_called = true
+      end
+      inner_called.should be_true
+      subject.current_sanitizer_scope.should == :before
+    end
+    
+  end
+  
+  context 'instance level scoping' do
+    
+    before :each do
+      subject.current_sanitizer_scope          = :class_level
+      subject_instance.current_sanitizer_scope = :instance_level
+    end
+    
+    it 'should use the instance scope if present' do
+      subject_instance.current_sanitizer_scope.should == :instance_level
+    end
+    
+    it 'should fallback to the class scope if the instance scope is not present' do
+      subject_instance.current_sanitizer_scope = nil
+      subject_instance.current_sanitizer_scope.should == :class_level
+    end
+    
+    it 'should fallback to default if no scope is set' do
+      subject_instance.current_sanitizer_scope = nil
+      subject.current_sanitizer_scope = nil
+      subject_instance.current_sanitizer_scope.should == :default
+    end
+
+    it 'should let you assign the instance level scope' do
+      subject_instance.current_sanitizer_scope = :after
+      subject_instance.current_sanitizer_scope.should == :after
+    end
+    
+  end
+  
+  context 'recognizers and converters' do
+    
+    before :each do
+      @sanitizer = ScopedAttrAccessible::Sanitizer.new
+      stub(subject).scoped_attr_sanitizer { @sanitizer }
+    end
+    
+    it 'should call the sanitizers methods when defining a converter' do
+      mock(@sanitizer).define_converter
+      subject.sanitizer_scope_converter {}
+    end
+    
+    it 'should call the sanitizers methods when defining a recognizer' do
+      mock(@sanitizer).define_recognizer(:admin)
+      subject.sanitizer_scope_recognizer(:admin) { }
+    end
+    
+  end
+  
+  context 'hooking into the process' do
+    
+    subject do
+      Class.new do
+        include ActiveModel::MassAssignmentSecurity
+        
+        attr_accessible :a, :b, :c
+        attr_accessible :a, :b, :c, :d, :scope => :owner
+        attr_accessible :all,           :scope => :admin
+        attr_protected :c, :d,          :scope => :guy_who_deletes_stuff
+        
+        def sanitized_keys_from(hash)
+          sanitize_for_mass_assignment(hash).keys
+        end
+      end
+    end
+    
+    let(:subject_instance) { subject.new }
+    
+    let :example_attributes do
+      {'a' => 'a', 'b' => 'c', 'c' => 'c', 'd' => 'd', 'e' => 'e'}
+    end
+    
+    it 'should return the correct attributes for the default scope' do
+      subject_instance.current_sanitizer_scope = nil
+      subject.current_sanitizer_scope          = nil
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b c)
+    end
+    
+    it 'should return the correct attributes for the owner scope' do
+      subject_instance.current_sanitizer_scope = :owner
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b c d)
+    end
+    
+    it 'should return the correct attributes for the admin scope' do
+      subject_instance.current_sanitizer_scope = :admin
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b c d e)
+    end
+    
+    it 'should return the correct attributes for an unknown scope' do
+      subject_instance.current_sanitizer_scope = :unknown_scope
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b c d e)
+    end
+    
+    it 'should return the correct attributes for the guy_who_deletes_stuff scope' do
+      subject_instance.current_sanitizer_scope = :guy_who_deletes_stuff
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b e)
+    end
+    
+  end
+  
+  context 'tying it all together' do
+    
+    subject do
+      Class.new do
+        include ActiveModel::MassAssignmentSecurity
+        
+        attr_accessible :a, :b, :c
+        attr_accessible :a, :b, :c, :d, :scope => :owner
+        attr_accessible :all,           :scope => :admin
+        attr_protected :c, :d,          :scope => :guy_who_deletes_stuff
+        
+        sanitizer_scope_recognizer :owner do |record, value|
+          value.is_a?(Numeric)
+        end
+        
+        sanitizer_scope_converter do |record, value|
+          return :admin if value == "hola"
+          return :guy_who_deletes_stuff if record.name == "Bob"
+        end
+        
+        attr_accessor :name
+        
+        def sanitized_keys_from(hash)
+          sanitize_for_mass_assignment(hash).keys
+        end
+      end
+    end
+    
+    let(:subject_instance) { subject.new }
+    
+    let :example_attributes do
+      {'a' => 'a', 'b' => 'c', 'c' => 'c', 'd' => 'd', 'e' => 'e'}
+    end
+    
+    before :each do
+      subject_instance.current_sanitizer_scope = nil
+      subject.current_sanitizer_scope          = nil
+
+    end
+    
+    it 'should return the correct attributes for the default scope' do
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b c)
+    end
+    
+    it 'should return the correct attributes for the owner scope' do
+      subject_instance.current_sanitizer_scope = 12
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b c d)
+    end
+    
+    it 'should return the correct attributes for the admin scope' do
+      subject_instance.current_sanitizer_scope = "hola"
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b c d e)
+    end
+    
+    it 'should return the correct attributes for the guy_who_deletes_stuff scope' do
+      subject_instance.name = "Bob"
+      subject_instance.current_sanitizer_scope = "trigger-it"
+      subject_instance.sanitized_keys_from(example_attributes).should == %w(a b e)
+      subject_instance.name = nil
+    end
+    
+    it 'should fallback if with a different sanitizer' do
+      klass = Class.new(subject)
+      klass._active_authorizer = nil
+      instance = klass.new
+      instance.sanitized_keys_from(example_attributes).should == %w(a b c d e)
+    end
+    
+    it 'should not overwrite children class sanitizers' do
+      klass = Class.new(subject)
+      klass.attr_accessible :e, :scope => :owner
+      subject.scoped_attr_sanitizer.sanitize_with_scope(example_attributes, :owner, nil).should only_have_attributes('a', 'b', 'c', 'd')
+    end
+    
+  end
+  
+end
